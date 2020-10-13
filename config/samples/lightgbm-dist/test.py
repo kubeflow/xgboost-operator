9b3af5a239e40c3401fcb8b4a93fbd834e1c7b07
Rene Delgado <rene854@gmail.com>
2020-10-13 18:10:39 -0300

    Clean unused files

--- config/samples/lightgbm-dist/test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import subprocess
-
-
-cmd = ["sh", "./test.sh"]
-
-proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-while (line := proc.stdout.readline()) :
-    print(line.decode("utf-8").strip())
